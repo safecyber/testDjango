2cc6c5857f7f04fce69e3a24e47f1c2690cc429c
safecyber <sabinafeenstra@gmail.com>
2024-12-04 11:09:50 +0100

    Added Voetbalspelers model and updated admin configuration

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,17 +1,17 @@
-from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
-class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+class Voetbalspelers(models.Model):
+    naam = models.CharField(max_length=100)
+    actuele_club = models.CharField(max_length=100)
+    auteur = models.ForeignKey(User, on_delete=models.CASCADE)
+    datum_invoer = models.DateTimeField(default=timezone.now)
+    datum_laatste_aanpassing = models.DateTimeField(auto_now=True)
 
-def publish(self):
-    self.published_date = timezone.now()
-    self.save()
+    def save(self, *args, **kwargs):
+        # Hier kun je extra acties uitvoeren vóór het opslaan
+        super().save(*args, **kwargs)
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.naam
